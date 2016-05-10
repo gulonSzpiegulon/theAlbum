278dfbe5380fa537fe044ce8bfd563a2b491cef9
Janek <j.gulkowski@wp.pl>
2016-05-11 01:49:52 +0200

    First subcomponent - login - with template and styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login.component';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My First Angular 2 App</h1>'
+	templateUrl: 'app/templates/app.html',
+	directives: [LoginComponent]
 })
 
 export class AppComponent {
---
+++ app/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'login',
+	templateUrl: 'app/templates/login.html',
+	styleUrls: ['app/styles/login.css']
+})
+
+export class LoginComponent {
+	
+}
